7beb084adc5b8718eeeabaab83d35d3fb27ddeac
dcoderk <webmarket2k@gmail.com>
2024-09-18 17:49:41 +0800

    adding model-factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
+    use HasFactory;
+    
     protected $table = 'job_listings';
 
     protected $fillable = [ 'title', 'salary' ];
