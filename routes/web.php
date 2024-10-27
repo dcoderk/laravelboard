598bb6f67baaf45c313fbe52e689d4be30e51569
dcoderk <webmarket2k@gmail.com>
2024-10-27 22:44:04 +0800

    JobController and working with Queue

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use \App\Jobs\TranslateJob;
+use \App\Models\Job;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('test', function () {
+    $job = Job::first();
+   TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
